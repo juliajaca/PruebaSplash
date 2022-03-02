630b0e83bdf740fca237e3c6546eecf6e26c1ae3
jjaca <jjaca@cifpfbmoll.eu>
2022-03-02 21:16:19 +0100

    empezado el recyclerview

---
+++ app/src/main/java/com/example/pruebasplash/PuntuacionAdapter.java
@@ -0,0 +1,101 @@
+package com.example.pruebasplash;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PuntuacionAdapter extends RecyclerView.Adapter<PuntuacionAdapter.ViewHolder>{
+    // Member variables.
+    private ArrayList<Puntuacion> mSportsData;
+    private Context mContext;
+
+    /**
+     * Constructor that passes in the sports data and the context.
+     *
+     * @param sportsData ArrayList containing the sports data.
+     * @param context Context of the application.
+     */
+    PuntuacionAdapter(Context context, ArrayList<Puntuacion> sportsData) {
+        this.mSportsData = sportsData;
+        this.mContext = context;
+    }
+
+
+    /**
+     * Required method for creating the viewholder objects.
+     *
+     * @param parent The ViewGroup into which the new View will be added
+     *               after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return The newly created ViewHolder.
+     */
+    @Override
+    public PuntuacionAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext).
+                inflate(R.layout.list_item, parent, false));
+    }
+
+    /**
+     * Required method that binds the data to the viewholder.
+     *
+     * @param holder The viewholder into which the data should be put.
+     * @param position The adapter position.
+     */
+    @Override
+    public void onBindViewHolder(PuntuacionAdapter.ViewHolder holder,
+                                 int position) {
+        // Get current sport.
+        Puntuacion currentSport = mSportsData.get(position);
+
+        // Populate the textviews with data.
+        holder.bindTo(currentSport);
+    }
+
+    /**
+     * Required method for determining the size of the data set.
+     *
+     * @return Size of the data set.
+     */
+    @Override
+    public int getItemCount() {
+        return mSportsData.size();
+    }
+
+
+    /**
+     * ViewHolder class that represents each row of data in the RecyclerView.
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Member Variables for the TextViews
+        private TextView mTitleText;
+        private TextView mInfoText;
+
+        /**
+         * Constructor for the ViewHolder, used in onCreateViewHolder().
+         *
+         * @param itemView The rootview of the list_item.xml layout file.
+         */
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            // Initialize the views.
+            mTitleText = itemView.findViewById(R.id.title);
+            mInfoText = itemView.findViewById(R.id.subTitle);
+        }
+
+        void bindTo(Puntuacion currentSport){
+            // Populate the textviews with data.
+            mTitleText.setText(currentSport.getTitle());
+            mInfoText.setText(currentSport.getInfo());
+
+        }
+    }
+}
--- app/src/main/java/com/example/pruebasplash/Scores.java
+++ app/src/main/java/com/example/pruebasplash/Scores.java
@@ -1,11 +1,18 @@
 package com.example.pruebasplash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TabHost;
 
+import java.util.ArrayList;
+
 public class Scores extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private ArrayList<Puntuacion> mSportsData;
+    private PuntuacionAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,45 @@ public class Scores extends AppCompatActivity {
         host.addTab(pegTab);
 
         host.setCurrentTabByTag("2048");
+
+        // Initialize the RecyclerView.
+        mRecyclerView = findViewById(R.id.ScrollViewAllScores);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the ArrayList that will contain the data.
+        mSportsData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new PuntuacionAdapter(this, mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the data.
+        initializeData();
+    }
+
+    /**
+     * Initialize the sports data from resources.
+     */
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] sportsList = getResources()
+                .getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources()
+                .getStringArray(R.array.sports_info);
+
+        // Clear the existing data (to avoid duplication).
+        mSportsData.clear();
+
+        // Create the ArrayList of Sports objects with titles and
+        // information about each sport.
+        for(int i=0;i<sportsList.length;i++){
+            mSportsData.add(new Puntuacion(sportsList[i],sportsInfo[i]));
+        }
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
     }
 }
